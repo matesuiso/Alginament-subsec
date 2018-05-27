5db2e33e1eddfca85e7a9b4cffd339471fd6c44a
matesuiso <matesuiso@hotmail.com>
2018-05-27 16:07:18 -0300

    asdd

--- Alignment.java
+++ Alignment.java
@@ -32,11 +32,11 @@ public class Alignment {
   }
 
 
-   public int getAlignment (String x, String y) throws Exception{
+  public int getAlignment (String x, String y) throws Exception{
      lengthx = x.length();
      lengthy = y.length();
 
-     String[] prueba = cosa(x, y);
+     String[] prueba = getStrings(x, y);
 
      System.out.println(prueba[0]);
      System.out.println(prueba[1]);
@@ -44,88 +44,88 @@ public class Alignment {
      return cost (x, y);
   }
 
- public String[] cosa (String x, String y) throws Exception{
+  private String[] getStrings (String x, String y) throws Exception{
     String res1 = "";
-    String res2 = "";
+      String res2 = "";
 
-    if (y.length() > 0 && x.length() > 0) {
+      if (y.length() > 0 && x.length() > 0) {
 
-      if (x.charAt(0) == y.charAt(0)) {
+         if (x.charAt(0) == y.charAt(0)) {
 
-         res1 += x.charAt(0);
-         res2 += y.charAt(0);
+            res1 += x.charAt(0);
+            res2 += y.charAt(0);
 
 
-         String [] tmp = cosa (cortar(x), cortar(y));
-         res1 += tmp[0];
-         res2 += tmp[1];
+            String [] tmp = getStrings (cortar(x), cortar(y));
+            res1 += tmp[0];
+            res2 += tmp[1];
 
-      } else {
-
-         // aca llamamos con el 'y' sin un elemento
-         String res11 = res1 + simbol;
-         String res12 = res2 + y.charAt(0);
-         String casiY = cortar(y);
-         String[] revisar1 = cosa(x, casiY);
-
-         // aca llamamos con el 'x' sin un elemento
-         String res21 = res1 + x.charAt(0);
-         String res22 = res2 + simbol;
-         String casiX = cortar(x);
-         String[] revisar2 = cosa (casiX, y);
-
-         // aca llamams al metodo, salteando ambos
-         String res31 = res1 + x.charAt(0);
-         String res32 = res2 + y.charAt(0);
-         String[] revisar3 = cosa (cortar(x), cortar(y));
-
-         int rev1,rev2,rev3;
-
-         // ver si costo en hash
-
-         try {
-            rev1 = cost(revisar1[0], revisar1[1]);
-            rev2 = cost(revisar2[0], revisar2[1]);
-            rev3 = cost(revisar3[0], revisar3[1]);
-         } catch(Exception e) {
-            throw new Exception ("Cost Method Failure.");
-         }
-
-         // aca guardar hash
-
-         if (rev1 < rev2) {
-           if (rev1 < rev3) {
-             res1 += res11 + revisar1[0];
-            res2 += res12 + revisar1[1];
-            } else {
-               res1 += res31 + revisar3[0];
-               res2 += res32 + revisar3[1];
-            }
          } else {
-           if (rev2 < rev3) {
-             res1 += res21 + revisar2[0];
-            res2 += res22 + revisar2[1];
-            } else {
-               res1 += res31 + revisar3[0];
-               res2 += res32 + revisar3[1];
+
+            // aca llamamos con el 'y' sin un elemento
+            String res11 = res1 + simbol;
+            String res12 = res2 + y.charAt(0);
+            String casiY = cortar(y);
+            String[] revisar1 = getStrings(x, casiY);
+
+            // aca llamamos con el 'x' sin un elemento
+            String res21 = res1 + x.charAt(0);
+            String res22 = res2 + simbol;
+            String casiX = cortar(x);
+            String[] revisar2 = getStrings (casiX, y);
+
+            // aca llamams al metodo, salteando ambos
+            String res31 = res1 + x.charAt(0);
+            String res32 = res2 + y.charAt(0);
+            String[] revisar3 = getStrings (cortar(x), cortar(y));
+
+            int rev1,rev2,rev3;
+
+            // ver si costo en hash
+
+            try {
+               rev1 = cost(revisar1[0], revisar1[1]);
+               rev2 = cost(revisar2[0], revisar2[1]);
+               rev3 = cost(revisar3[0], revisar3[1]);
+            } catch(Exception e) {
+               throw new Exception ("Cost Method Failure.");
             }
-         }
 
-      }
-   } else {
-      if (y.length() == 0) {
-         for (int k = 0; k < x.length() ; k++) {
-            res2 += simbol;
+            // aca guardar hash
+
+            if (rev1 < rev2) {
+               if (rev1 < rev3) {
+                  res1 += res11 + revisar1[0];
+                  res2 += res12 + revisar1[1];
+               } else {
+                  res1 += res31 + revisar3[0];
+                  res2 += res32 + revisar3[1];
+               }
+            } else {
+               if (rev2 < rev3) {
+                  res1 += res21 + revisar2[0];
+                  res2 += res22 + revisar2[1];
+               } else {
+                  res1 += res31 + revisar3[0];
+                  res2 += res32 + revisar3[1];
+               }
+            }
+
          }
-         res1 += x;
       } else {
-         for (int k = 0; k < y.length() ; k++) {
-            res1 += simbol;
+         if (y.length() == 0) {
+            for (int k = 0; k < x.length() ; k++) {
+               res2 += simbol;
+            }
+            res1 += x;
+         } else {
+            for (int k = 0; k < y.length() ; k++) {
+               res1 += simbol;
+            }
+            res2 += y;
          }
-         res2 += y;
-      }
 
-  }
+      }
 
     String[] result = {res1, res2};
     return result;
